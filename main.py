dc3ef1c9ee17f212dbcf764bf430ce5a1a7d0aa8
aghaeihesam57-delhmgjyfytfjytdhttyhy <aghaeihesam57@gmail.com>
2026-05-30 05:45:33 +0330

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+import os
+import asyncio
+from aiogram import Bot, Dispatcher
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher()
+
+@dp.message(CommandStart())
+async def start(message: Message):
+    await message.answer("MZ-01 فعال شد 🍌")
+
+@dp.message()
+async def echo(message: Message):
+    await message.answer("پیام دریافت شد.")
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
